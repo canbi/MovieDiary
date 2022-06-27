0b4f2435d0ffa36d6c22ddf28510a923ca1af697
Can Bi <bica@mef.edu.tr>
2022-06-27 08:11:21 +0300

    Initial Commit

---
+++ MovieDiary/MovieDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDiaryApp.swift
+//  MovieDiary
+//
+//  Created by Can Bi on 27.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
